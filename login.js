e1cbbfbbf55a4e980b6719902e0e780a70f60888
eleanorpatel <eleanor.v.patel@gmail.com>
2026-08-19 16:16:46 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,20 @@
+const signUpButton = document.getElementById("Signup")
 
+signUpButton.addEventListener("click", =>{
+ window.location.href = "signup.html" 
+}) 
+
+const username = document.getElementById("username")
+const password = document.getElementById("password")
+const signInButton = document.getElementById("signin")
+
+function signIn(){
+  if username == "" or password == ""{
+    alert("all fields must be filled in")
+  }
+}
+
+signUpButton.addEventListener("click", =>{
+  signin()
+    window.location.href = "index.html"
+})
